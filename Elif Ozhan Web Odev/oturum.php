93ce07393ebc576ac548d84112c91be9e036f467
Elif Özhan <elif.ozhan1@ogr.sakarya.edu.tr>
2023-05-17 19:12:50 +0300

    addings in the pages and fixing the bugs

--- Elif Ozhan Web Odev/oturum.php
+++ Elif Ozhan Web Odev/oturum.php
@@ -1,37 +1,14 @@
 <?php
-// login.php
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+}
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+if ($username == 'b221210001@sakarya.edu.tr' && $password == 'b221210001') {
+    
+    header("Location: hosgeldiniz.html");
+} else {
 
-  // Check if both fields are filled
-  if(empty($username) || empty($password)) {
-    header("Location: login.php");
-    exit;
-  }
-
-  // Check if username is a valid email address
-  if(!filter_var($username, FILTER_VALIDATE_EMAIL)) {
-    header("Location: login.php");
-    exit;
-  }
-
-  // Check if username and password are correct
-  $correct_username = "b221210001@sakarya.edu.tr";
-  $correct_password = "b221210001";
-
-  if($username == $correct_username && $password == $correct_password) {
-    echo ("Location: hosgeldiniz.html");
-  } else {
-    header("Location: oturum.html");
-    exit;
-  }
+    header("Location: hata.html");
 }
 ?>
-
-<form method="POST" action="login.php">
-  Kullanıcı Adı: <input type="text" name="username"><br>
-  Şifre: <input type="password" name="password"><br>
-  <input type="submit" value="Giriş Yap">
-</form>
